510dd6bf2e15a4cc6b3edd7f07f9cbb03ba1eefc
Leorio21 <Ptitlue@gmail.com>
2024-07-24 13:54:32 +0200

    add api_url

--- services/queries.ts
+++ services/queries.ts
@@ -3,7 +3,7 @@ import { InvoiceScheme, InvoicesScheme } from "./scheme";
 export const getInvoices = () => ({
   queryKey: ["invoices"],
   queryFn: async () => {
-    const res = await fetch("http://localhost:3000/api/invoices");
+    const res = await fetch(`${process.env.BASE_URL}/invoices`);
     const data = await res.json();
     return InvoicesScheme.parse(data.invoices);
   },
@@ -12,7 +12,7 @@ export const getInvoices = () => ({
 export const getInvoice = (id: string) => ({
   queryKey: ["invoice", id],
   queryFn: async () => {
-    const res = await fetch(`http://localhost:3000/api/invoices/${id}`);
+    const res = await fetch(`${process.env.BASE_URL}/invoices/${id}`);
     const data = await res.json();
     console.log(data);
     console.log(InvoiceScheme.parse(data.invoice));
