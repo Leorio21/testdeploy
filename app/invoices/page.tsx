1acbcafb30c7a3caf14de1933fe623084747b1be
Leorio21 <Ptitlue@gmail.com>
2024-07-31 10:24:03 +0200

    clean code

--- app/invoices/page.tsx
+++ app/invoices/page.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import Invoices from "../Components/Invoices/Invoices";
 import { auth } from "@/lib/auth";
 
-const page = async () => {
-  const session = await auth();
-  console.log(session?.user);
-  console.log(process.env.API_URL);
+const page = () => {
   return <Invoices />;
 };
 
